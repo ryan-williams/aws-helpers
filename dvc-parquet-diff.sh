8e9639fe5b005935de7c76782425609d672f856d
Ryan Williams <ryan@runsascoded.com>
2024-12-21 09:03:37 -0500

    `dvc-parquet-diff.sh`: handle adds/deletes

--- dvc-parquet-diff.sh
+++ dvc-parquet-diff.sh
@@ -6,10 +6,18 @@ if [ "$#" -ne 7 ]; then
 fi
 
 path="$1"; shift  # repo relpath
-pqt0="$(dvc_local_cache_path -r "$1")"; shift
+if [ "$1" == /dev/null ]; then
+  pqt0=/dev/null; shift
+else
+  pqt0="$(dvc_local_cache_path -r "$1")"; shift
+fi
 hex0="$1"; shift  # old hexsha
 mode0="$1"; shift  # old filemode
-pqt1="$(dvc_local_cache_path -r "$1")"; shift
+if [ "$1" == /dev/null ]; then
+  pqt1=/dev/null; shift
+else
+  pqt1="$(dvc_local_cache_path -r "$1")"; shift
+fi
 hex1="$1"; shift  # new hexsha
 mode1="$1"; shift  # old filemode
 
